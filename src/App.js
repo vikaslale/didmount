3d81eeb3195483d70235853423ce9270b1493690
vikas lale <vikaslalevpl@gmail.com>
2021-04-30 14:37:45 +0530

    didmount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+class App extends React.Component{
+  constructor()
+  {
+    super()
+    this.state={
+      data:null
+    }
+    console.warn("constructor")
+  }
+  componentDidMount(){
+    this.setState({data:"updated"});
+    console.warn("componentDidMount")
+  }
+  render(){
+    console.warn("render")
+    return(
+      <div>
+ <h1> |Life cycle Method: ComponentDidMount :)</h1>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
